9e5b4bfc3d4254dc043e3f507c287383b6acd7c9
Narahari Hegde <116895826+NarahariHegde@users.noreply.github.com>
2023-06-04 18:42:18 +0530

    Add files via upload

---
+++ controller/UserController.js
@@ -0,0 +1,30 @@
+const usermodel = require("../model/UserModel");
+
+const registerUser = async (req, res) => {
+  try {
+    const newuser = new usermodel(req.body);
+    await newuser.save();
+    res.status(201).json({ sucess: true, newuser });
+  } catch (error) {
+    res.status(400).send("invalid operation");
+    console.log(error);
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await usermodel.findOne({ email, password });
+    if (!user) {
+      res.status(400).send("error in login");
+      console.log("login failed");
+    } else {
+      res.status(200).json({ success: true, user });
+      console.log("login sucessfull");
+    }
+  } catch (error) {
+    res.status(400).send("invalid credentials");
+    console.log(error);
+  }
+};
+module.exports = { registerUser, loginUser };
